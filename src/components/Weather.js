a4bfa82656fde8bf48b18225173ce65e83a82291
Nishchint Dhawan <nishchint34@gmail.com>
2020-02-09 01:27:16 -0800

    updated colors

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -10,20 +10,20 @@ const Weather = props => {
                 <div className="col-6">
                     {props.city && props.country &&
                         <div className="weather-info card text-center text-light bg-dark">
-                            <p className="weather-location-title">{props.city}, {props.country} </p>
+                            <p className="weather-location-title"> <span className="weather-parameter-caption">{props.city}, {props.country} </span></p>
                             {props.temperature && <p className="weather-temperature-title"> {props.temperature} {'\u00b0'}C</p>}
-                            {props.feels_like && <p className="weather-feels-like">Feels Like: {props.feels_like} {'\u00b0'}C</p>}
+                            {props.feels_like && <p className="weather-feels-like"> <span className="weather-parameter-caption"> Feels Like </span> :  {props.feels_like} {'\u00b0'}C</p>}
                         </div>
                     }
                 </div>
                 <div className="col-6">
                     {props.city && props.country &&
                         <div className="weather-info card text-center text-light bg-dark">
-                            <p className="weather-parameter">Maximum Temperature: {props.min_temp} {'\u00b0'}C </p>
-                            <p className="weather-parameter">Minimum Temperature: {props.max_temp} {'\u00b0'}C  </p>
-                            <p className="weather-parameter">Humidity: {props.humidity} {'\u0025'} </p>
-                            <p className="weather-parameter">Conditions: {props.description} </p>
-                            <p className="weather-parameter">Wind: {props.wind} km/hr </p>
+                            <p className="weather-parameter"> <span className="weather-parameter-caption">Max</span> :  {props.min_temp} {'\u00b0'}C </p>
+                            <p className="weather-parameter"> <span className="weather-parameter-caption">Min</span> : {props.max_temp} {'\u00b0'}C  </p>
+                            <p className="weather-parameter"> <span className="weather-parameter-caption">Humidity </span> : {props.humidity} {'\u0025'} </p>
+                            <p className="weather-parameter"> <span className="weather-parameter-caption">Conditions </span> : {props.description} </p>
+                            <p className="weather-parameter"> <span className="weather-parameter-caption">Wind </span> : {props.wind} km/hr </p>
                         </div>
                     }
                 </div>
